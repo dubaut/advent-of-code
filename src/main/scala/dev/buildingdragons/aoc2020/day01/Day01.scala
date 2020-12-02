d766d031c2aeae3f7ca47785a91a720b324319e0
Hannes Halenka <hannes@transentials.com>
2020-12-02 20:41:52 +0100

    Solve day 01, part 2

--- src/main/scala/dev/buildingdragons/aoc2020/day01/Day01.scala
+++ src/main/scala/dev/buildingdragons/aoc2020/day01/Day01.scala
@@ -2,41 +2,47 @@ package dev.buildingdragons.aoc2020.day01
 
 import dev.buildingdragons.aoc2020.Util
 
+import scala.annotation.tailrec
+
 object Day01 {
-  def findSummands(expanseReport: List[Int]): (Int, Int) = {
-    (1 until expanseReport.length).map { index =>
-      val value = expanseReport(index)
-      val values = expanseReport.filter(_ != value)
-      (value, values)
-    }.flatMap { item =>
-      item._2.map { x =>
-        val list = (item._1 :: x :: Nil).sorted
-        (list.head, list(1))
+  def summands(expanses: List[Int], n: Int): List[Int] = group(expanses)(n).filter(_.sum == 2020).head
+
+  @tailrec
+  def group(input: List[Int], groups: List[List[Int]] = Nil)(n: Int): List[List[Int]] = {
+    if (groups.isEmpty) {
+      group(input, input.map(_ :: Nil))(n)
+    } else {
+      val result = groups.flatMap { element =>
+        (input diff element).map { x =>
+          (element ++ List(x)).sorted
+        }
+      }.distinct
+
+      if (result.head.length == n) {
+        result
+      } else {
+        group(input, result)(n)
       }
-    }.filter(value => value._1 + value._2 == 2020).head
+    }
   }
 
-  def solvePart1(expanses: List[Int]): Int = {
-    val summands = findSummands(expanses)
-    summands._1 * summands._2
-  }
+  def solvePart1(expanses: List[Int]): Int = summands(expanses, 2).product
+
+  def solvePart2(expanses: List[Int]): Int = summands(expanses, 3).product
 
   def answer: Either[Any, Result] = {
-    val part1 = Util.loadTextFileResource("day01/input.txt").toEither match {
+    Util.loadTextFileResource("day01/input.txt").toEither match {
       case Left(throwable) => Left(throwable)
       case Right(lines) =>
         val expanses = lines.map(_.toInt)
-        val summands = findSummands(expanses)
-        Right(summands._1 * summands._2)
-    }
 
-    part1 match {
-      case Left(value) => Left(value)
-      case Right(part1) =>
-        Right(Result(part1))
+        val part1 = solvePart1(expanses)
+        val part2 = solvePart2(expanses)
+
+        Right(Result(part1, part2))
     }
   }
 
-  final case class Result(part1: Int)
+  final case class Result(part1: Int, part2: Int)
 
 }
--- src/test/scala/dev/buildingdragons/aoc2020/day01/Day01Test.scala
+++ src/test/scala/dev/buildingdragons/aoc2020/day01/Day01Test.scala
@@ -1,21 +1,38 @@
 package dev.buildingdragons.aoc2020.day01
 
-import dev.buildingdragons.aoc2020.day01.Day01.{answer, findSummands, solvePart1}
+import dev.buildingdragons.aoc2020.day01.Day01.{summands, group, solvePart1, solvePart2}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 
 class Day01Test extends AnyFlatSpec with Matchers {
-  "findSummands" must "must return those two elements that add up to 2020." in {
-    val expanseReport = List(366, 979, 1721, 299, 675, 1456)
+  "summands" must "must return those two elements that add up to 2020." in {
+    val expenses = List(366, 979, 1721, 299, 675, 1456)
 
-    assertResult((299, 1721)) {
-      findSummands(expanseReport)
+    val expected = List(1721, 299)
+    val actual = summands(expenses, 2)
+
+    actual must contain only (expected: _*)
+  }
+
+  "group" must "return number groups." in {
+    val input = List(1, 2, 3)
+    val n = 2
+
+    val expected = List(List(1, 2), List(1, 3), List(2, 3))
+    assertResult(expected) {
+      group(input)(n)
     }
   }
 
   "solvePart1" must "return 514579 for 366, 979, 1721, 299, 675, and 1456." in {
     assertResult(514579) {
       solvePart1(List(366, 979, 1721, 299, 675, 1456))
+    }
+  }
+
+  "solvePart2" must "return 241861950 for 366, 979, 1721, 299, 675, and 1456." in {
+    assertResult(241861950) {
+      solvePart2(List(366, 979, 1721, 299, 675, 1456))
     }
   }
 }
